56a428658e09eea4d034575fa5a949ee7739377a
rusdaii <rusdaii.html@gmail.com>
2024-04-26 21:33:22 +0700

    style: delete Auth Illustratition image

--- src/components/pages/Auth/index.tsx
+++ src/components/pages/Auth/index.tsx
@@ -1,8 +1,5 @@
 import dynamic from 'next/dynamic';
-import Image from 'next/image';
 
-import signInIllustration from '@/assets/illustrations/sign_in.svg?url';
-import signUpIllustration from '@/assets/illustrations/sign_up.svg?url';
 const SigninForm = dynamic(() => import('@/components/parts/Form/SigninForm'));
 const SignupForm = dynamic(() => import('@/components/parts/Form/SignupForm'));
 import { Link } from '@/components/parts/Link';
@@ -21,14 +18,6 @@ const Auth = ({ page }: AuthProps) => {
   return (
     <div className="w-full pt-16 flex justify-center items-center">
       <Card className="w-full max-w-md">
-        <Image
-          src={page === 'login' ? signInIllustration : signUpIllustration}
-          width={800}
-          height={800}
-          quality={100}
-          alt="Sign in Illustration"
-          className="w-[250px] h-[250px] mx-auto"
-        />
         <CardHeader>
           <h1 className="text-2xl font-semibold text-center">
             {page === 'login' ? 'Sign in' : 'Sign up'}
