be13c13a4d2b40f6c40097da0f869b49d5c18faf
rusdaii <rusdaii.html@gmail.com>
2024-04-26 21:49:55 +0700

    feat: add warning payment at checkout page

--- src/components/pages/Checkout/index.tsx
+++ src/components/pages/Checkout/index.tsx
@@ -1,7 +1,11 @@
 'use client';
 import { useEffect } from 'react';
 
+import { PiWarningCircle } from 'react-icons/pi';
+
 import CheckoutComponent from '@/components/parts/Checkout/CheckoutComponent';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Button } from '@/components/ui/button';
 import { clientKey, snapUrl } from '@/lib/constants/midtrans';
 
 const Checkout = () => {
@@ -20,7 +24,31 @@ const Checkout = () => {
   }, []);
 
   return (
-    <section className="container py-10">
+    <section className="container py-5">
+      <Alert className="mb-5" variant="destructive">
+        <PiWarningCircle className="h-5 w-5" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          <span>
+            This is a demo website. Please do not use real credit card
+            information.
+            <br />
+            To test the payment gateway, please use the simulator provided
+            below.
+            <br />
+          </span>
+          <a
+            href="https://simulator.sandbox.midtrans.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:text-red-800 mt-10"
+          >
+            <Button variant="link" size="sm">
+              Click here to open the simulator
+            </Button>
+          </a>
+        </AlertDescription>
+      </Alert>
       <CheckoutComponent />
     </section>
   );
