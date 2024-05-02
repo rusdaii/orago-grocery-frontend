07175212d542454c9ed7e8b10c51925efb6e02da
rusdaii <rusdaii.html@gmail.com>
2024-05-02 17:43:33 +0700

    feat: add footer link url

--- src/lib/constants/FooterLink.ts
+++ src/lib/constants/FooterLink.ts
@@ -2,28 +2,27 @@ export const FooterLink = [
   {
     name: 'My Account',
     links: [
-      { name: 'Profile', href: '#' },
-      { name: 'Order History', href: '#' },
-      { name: 'Shopping Cart', href: '#' },
-      { name: 'Wishlist', href: '#' },
+      { name: 'Profile', href: '/account/dashboard' },
+      { name: 'Order History', href: '/account/orders-history' },
+      { name: 'Shopping Cart', href: '/shopping-cart' },
+      { name: 'Wishlist', href: '/wishlist' },
     ],
   },
   {
     name: 'Helps',
     links: [
-      { name: 'Contact', href: '#' },
-      { name: 'FAQ', href: '#' },
-      { name: 'Terms & Conditions', href: '#' },
-      { name: 'Privacy Policy', href: '#' },
+      { name: 'Contact', href: '/contact' },
+      { name: 'FAQ', href: '/faq' },
+      { name: 'Terms & Conditions', href: '/terms-condition' },
+      { name: 'Privacy Policy', href: '/privacy' },
     ],
   },
   {
     name: 'Proxy',
     links: [
-      { name: 'About', href: '#' },
-      { name: 'Shop', href: '#' },
-      { name: 'Product', href: '#' },
-      { name: 'Track Order', href: '#' },
+      { name: 'About', href: '/about' },
+      { name: 'Shop', href: '/shop' },
+      { name: 'Track Order', href: '/account/orders-history' },
     ],
   },
 ];
